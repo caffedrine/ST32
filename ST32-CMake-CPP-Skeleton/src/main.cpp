e7fd0bfd7a34085c4828b854d611d08dddd72696
caffedrine <bacchusstyle@yahoo.com>
2018-12-31 15:51:34 +0200

    added all st32discovery leds as demo

--- ST32-CMake-CPP-Skeleton/src/main.cpp
+++ ST32-CMake-CPP-Skeleton/src/main.cpp
@@ -1,5 +1,36 @@
 #include "derivative.h"
 
+class PushButton
+{
+	enum PushButtonState
+	{
+		DOWN = 0,
+		UP = 1
+	};
+	
+public:
+	PushButton(GPIO_TypeDef *LedPortRegister, uint32_t LedPortBit) : _PortRegister(LedPortRegister), _PortBit(LedPortBit)
+	{
+	}
+	
+	PushButtonState ReadState()
+	{
+		return PushButtonState::DOWN;
+	}
+	
+	void Tick()
+	{
+	
+	}
+	
+protected:
+	/// TODO: Implement
+	virtual void OnStateChanged(PushButtonState NewState) {}
+private:
+	GPIO_TypeDef *_PortRegister;
+	uint32_t _PortBit;
+};
+
 class Led
 {
 public:
@@ -19,6 +50,9 @@ public:
 		GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
 		GPIO_InitStruct.Pull = LL_GPIO_PULL_DOWN;
 		LL_GPIO_Init(_PortRegister, &GPIO_InitStruct);
+		
+		/* Led Off by default */
+		this->Off();
 	}
 
 	void On()
@@ -43,11 +77,36 @@ private:
 
 int main()
 {
-	Led led(GPIOE, LL_GPIO_PIN_9);
+	Led LedBlue(GPIOE, LL_GPIO_PIN_8);
+	Led LedRed(GPIOE, LL_GPIO_PIN_9);
+	Led LedOrange(GPIOE, LL_GPIO_PIN_10);
+	Led LedGreen (GPIOE, LL_GPIO_PIN_11);
+	
+	Led LedBlue2(GPIOE, LL_GPIO_PIN_12);
+	Led LedRed2(GPIOE, LL_GPIO_PIN_13);
+	Led LedOrange2(GPIOE, LL_GPIO_PIN_14);
+	Led LedGreen2(GPIOE, LL_GPIO_PIN_15);
+	
+	LedRed.On();
+	LedBlue.On();
+	LedOrange.On();
+	LedGreen.On();
+	LedRed2.Off();
+	LedBlue2.Off();
+	LedOrange2.Off();
+	LedGreen2.Off();
 	
 	while(true)
 	{
-		led.Toggle();
+		LedRed.Toggle();
+		LedBlue.Toggle();
+		LedOrange.Toggle();
+		LedGreen.Toggle();
+		LedRed2.Toggle();
+		LedBlue2.Toggle();
+		LedOrange2.Toggle();
+		LedGreen2.Toggle();
+		
 		for(int i = 0; i<= 100000; i++);
 	}
 }
